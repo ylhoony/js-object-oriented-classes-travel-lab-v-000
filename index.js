b5d0bc686e559d7fdbb4d6767a8699140e67efcf
Hoon Lee <yl.hoony@gmail.com>
2017-10-06 21:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  class Driver {
    constructor(name, startDate) {
      this.name = name;
-     this.startDate = Date.parse(startDate);
+     this.startDate = IPOdate.setTime(Date.parse(startDate));
    }
  }
