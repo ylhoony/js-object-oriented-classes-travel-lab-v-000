253a876acf802af49ef3ace0386c094cbe847f31
Hoon Lee <yl.hoony@gmail.com>
2017-10-06 22:02:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
      this.startDate = new Date(startDate);
    }
    yearsExperienceFromBeginningOf(year) {
-     return this.startDate.getFullYear() - year;
+     return year - this.startDate.getFullYear();
    }
  }
